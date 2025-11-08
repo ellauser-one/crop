33a52a58812476e05754031f4fe961294be75b7f
ellauser-one <o1253356687@gmail.com>
2025-11-08 22:42:00 +0800

    soft and hard route

--- src/app/default.tsx
+++ src/app/default.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div className='flex mt-6 p-6 h-32 bg-gray-500 text-white rounded-lg'>
-      @children
+      @children default
     </div>
   )
 }
